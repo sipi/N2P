bdda91ab8b91ce5b113faea7d144405ac37c7106
Clément SIPIETER <c.sipieter@gmail.com>
2012-04-17 15:20:17 +0200

    Add method in Graph interface

--- src/pi/util/Graph.java
+++ src/pi/util/Graph.java
@@ -5,6 +5,6 @@ public interface Graph
   
   public void setNbrVertices(int nbrVertices);
   public void addArc(int idVertexSrc, int idVertexDest);
-  
-  
+  public String toString();
+  public boolean equals(Object obj);
 }
